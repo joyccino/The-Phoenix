999e630e9740e3646b4114e09ec064db6a17ad49
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-03-18 14:16:24 +0900

    mayo project

---
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/QpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.qpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpProjectApplication.class, args);
+	}
+
+}
