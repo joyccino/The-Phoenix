4f5a87cb32d923d0e2d45ec516fe0857f4a65a4e
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-03-18 19:44:42 +0900

    qp mayo project 뼈대

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/QpProjectApplication.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/QpProjectApplication.java
@@ -2,8 +2,10 @@ package com.phoenix.qpproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class QpProjectApplication {
 
 	public static void main(String[] args) {
