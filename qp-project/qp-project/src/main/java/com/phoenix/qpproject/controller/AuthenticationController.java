b6d07c83f89dd383a071836ece1b0e664fd62e9b
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-03-28 11:16:21 +0900

    회원가입 페이지, 비밀번호 찾기 페이지 url 매핑 완료

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/AuthenticationController.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/AuthenticationController.java
@@ -9,9 +9,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AuthenticationController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String test() {
+    public String login() {
         return "/pages/authentication/card/login";
     }
+
+    @RequestMapping(value = "authentication/register", method = RequestMethod.GET)
+    public String registerDefault() {
+        return "/pages/authentication/card/register";
+    }
+
+    @RequestMapping(value = "authentication/forgotPassword", method = RequestMethod.GET)
+    public String forgotPassword() {
+        return "/pages/authentication/card/forgot-password";
+    }
+
+
 
 
 }
