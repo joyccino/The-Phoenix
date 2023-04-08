b99f1afccca6fd2758ff474577b4585b934d416d
Alice Woo <48823257+alicewoo0925@users.noreply.github.com>
2023-04-08 13:41:59 +0900

    Revert "mybatis + mysql conn"

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/AuthenticationController.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/AuthenticationController.java
@@ -1,27 +1,12 @@
 package com.phoenix.qpproject.controller;
 
-import com.phoenix.qpproject.dto.MemberDTO;
-import com.phoenix.qpproject.mapper.MemberMapper;
-import com.phoenix.qpproject.service.MemberService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @Controller
 @RequestMapping("/")
 public class AuthenticationController {
-    private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
-
-    @Autowired
-    public MemberService memberService;
-
     @RequestMapping(method = RequestMethod.GET)
     public String login() {
         return "/pages/authentication/card/login";
@@ -42,15 +27,4 @@ public class AuthenticationController {
         return "/pages/quiz/quiz_form";
     }
 
-    @GetMapping(value="/memberList")
-    public List<MemberDTO> getMemberList(Model model) {
-        List<MemberDTO> memberList = memberService.getMemberList();
-
-        System.out.println("회원 목록을 요청합니다: "+memberList.toString());
-
-        model.addAttribute("title", "회원목록조회");
-        model.addAttribute("memberList", memberList);
-
-        return memberList;
-    }
 }
