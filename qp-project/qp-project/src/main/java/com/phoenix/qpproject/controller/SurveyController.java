3fc1c4d3b5a417543802780c1c210d828ee838e8
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-04-16 19:33:42 +0900

    퀴즈 & 설문조사 대시보드 뼈대

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/SurveyController.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/SurveyController.java
@@ -2,8 +2,14 @@ package com.phoenix.qpproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/survey")
 public class SurveyController {
+
+    @RequestMapping(value = "dashboard", method = RequestMethod.GET)
+    public String surveyDashboard() {
+        return "/surveyDashboard";
+    }
 }
