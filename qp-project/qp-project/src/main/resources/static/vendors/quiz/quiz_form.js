19d2d9fcbf9b6657c61265c8df117129cf177a21
GilbertMoon <jemoon222@gmail.com>
2023-04-21 00:26:18 +0900

    quiz_form.html - 퀴즈유형 선택하는 버튼 생성. 해당 버튼들 클릭하면 하단에 생성 & 삭제가능. 한가지 유형을 버튼 클릭하면 그 유형만 클릭 가능, 나머지 버튼들은 비활성화됨. 내용 다 삭제시 나머지 버튼들 활성화
    - 질문목록은 기능구현 x 화면구현만 되어있음.

--- qp-project/qp-project/src/main/resources/static/vendors/quiz/quiz_form.js
+++ qp-project/qp-project/src/main/resources/static/vendors/quiz/quiz_form.js
@@ -1,1 +1,27 @@
+   var quesBtn = document.getElementsByClassName("quesBtn");
 
+      function handleClick(event) {
+        console.log(event.target);
+        // console.log(this);
+        // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+        console.log(event.target.classList);
+
+        if (event.target.classList[1] === "clicked") {
+          event.target.classList.remove("clicked");
+        } else {
+          for (var i = 0; i < quesBtn.length; i++) {
+            quesBtn[i].classList.remove("clicked");
+          }
+
+          event.target.classList.add("clicked");
+        }
+      }
+
+      function init() {
+        for (var i = 0; i < quesBtn.length; i++) {
+          quesBtn[i].addEventListener("click", handleClick);
+        }
+      }
+
+      init();
