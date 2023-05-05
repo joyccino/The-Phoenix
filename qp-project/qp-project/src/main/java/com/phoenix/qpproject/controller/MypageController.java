a1a00fa7eac8d0264d7dec8dafb624e5ae2291d8
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-05-05 17:52:58 +0900

    mypage 개인정보 로딩

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/MypageController.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/MypageController.java
@@ -1,11 +1,13 @@
 package com.phoenix.qpproject.controller;
 
+import com.phoenix.qpproject.dto.MembersDTO;
 import com.phoenix.qpproject.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +23,7 @@ public class MypageController {
     public MemberService memberService;
 
     @RequestMapping(value = "edit", method = RequestMethod.GET)
-    public String edit( HttpServletRequest request) {
+    public String edit( HttpServletRequest request, Model model) {
         HttpSession session = request.getSession();
         Object qpUser = session.getAttribute("qpUser");
 
@@ -30,6 +32,7 @@ public class MypageController {
             return "/pages/authentication/card/login";
         }
         else {
+            model.addAttribute("qpUser", qpUser);
             return "/pages/user/settings.html";
         }
 
