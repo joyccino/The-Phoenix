9ec69073e514b1889d5416933f494450654c2aa5
home <alicewoo0925@gmail.com>
2023-05-06 22:12:29 +0900

    셋팅페이지 db에서 과목불러오기 업데이트 - 컨트롤러,캡퍼,서비스,html 업데이트 (아직 수정 필요)

--- qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/QuizController.java
+++ qp-project/qp-project/src/main/java/com/phoenix/qpproject/controller/QuizController.java
@@ -9,8 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,10 +35,20 @@ public class QuizController {
 
     @RequestMapping(value = "settingQuiz", method = RequestMethod.GET)
     public String quizsetting(Model model) {
-        List<SubjectsDTO> subjectList = subjectsService.getSubjectList();
-        System.out.println("조회된 과목들 수: "+subjectList.size());
-        model.addAttribute("subjectList", subjectList);
+        List<SubjectsDTO> generalSubjectList = subjectsService.getGeneralSubjectList();
+        System.out.println("조회된 과목들 수: "+generalSubjectList.size());
+        model.addAttribute("generalSubjectList", generalSubjectList);
         return "/pages/quiz/quiz_setting";
+    }
+
+    @PostMapping(value="getDetailSubjects")
+    @ResponseBody
+    public List<SubjectsDTO> getDetailSubjects(@RequestParam("selectedGeneralSubject") String selectedGeneralSubject, Model model) {
+        List<SubjectsDTO> detailSubjectList = subjectsService.getDetailSubjectList(selectedGeneralSubject);
+        System.out.println("조회된 세부 과목들 수: "+detailSubjectList.size());
+        System.out.println("넘겨온값 " + selectedGeneralSubject);
+        //model.addAttribute("detailSubjectList", detailSubjectList);
+        return detailSubjectList;
     }
 
 
